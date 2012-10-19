1950b50be7a5b5d11ce18b1176d9934c9dc447ca
Chris Coldren <coldrecd@gmail.com>
2012-10-19 17:00:38 -0500

    github testing

--- calc.py
+++ calc.py
@@ -5,4 +5,14 @@ for line in open(sys.argv[1]):
    num = int(line)
    x.append(num)
 
+avg = sum(x) / float(len(x))
+
+diffs = []
+for num in x:
+   diff = num - avg
+   diffs.append(diff**2)
+
+
 print 'average is', sum(x) / float(len(x))
+print 'sumsqdiffs is', sum(diffs)
+print "hello world"
